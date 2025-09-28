8867de9c4f5af47a0d9ad99a1573dd9e0e339492
shabir <shabirsediqi124@gmail.com>
2025-09-28 13:23:16 +0430

    Solved repetition problem

--- competitive_programming/problemSet/repetition.py
+++ competitive_programming/problemSet/repetition.py
@@ -1,8 +1,19 @@
 
 def longest_substring(s: str) -> int:
-    pass
+    count = 1
+    count_hash = {}
 
+    for char in range(len(s)-1):
+        if s[char] == s[char + 1]:
+            count += 1
+        else:
+            count_hash[char] = count
+            count = 1
+    count_hash[s[::-1]] = count
 
+    max_value = max(count_hash.values())
+    return max_value
 
+s = input()
 
-print(longest_substring("AAAAAAAAAA"))
+print(longest_substring(s))
